df52a87aff79a0b26d05537caa28bc5fe5ffab3b
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-01-29 05:34:42 +0900

    機能追加

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,19 +1,59 @@
 import React from 'react';
+import { textSpanContainsTextSpan } from 'typescript';
 
 interface Props {
   questionOpen: boolean;
-  question: string;
+  title: string;
+  datas: string[];
+  text: string;
   backTop: VoidFunction;
 }
 
-const Question: React.FC<Props> = ({ backTop, questionOpen, question }) => {
+const Question: React.FC<Props> = ({ questionOpen, title, datas, backTop }) => {
   if (!questionOpen) {
     return null;
   }
 
+  const getQuestion = () => {
+    console.log('getQuestion');
+    if (title === 'Computer Science') {
+      return datas.Computer[0].question;
+    } else if (title === 'Finance') {
+      return datas.Finance[0].question;
+    } else if (title === 'Game') {
+      return datas.Game[0].question;
+    } else {
+      return datas.History[0].question;
+    }
+  };
+
+  const getTexts = () => {
+    console.log('getTexts');
+    if (title === 'Computer Science') {
+      return datas.Computer[0].answers.map((data) => (
+        <div key={data.toString()}>{data}</div>
+      ));
+    } else if (title === 'Finance') {
+      return datas.Finance[0].answers.map((data) => (
+        <div key={data.toString()}>{data}</div>
+      ));
+    } else if (title === 'Game') {
+      return datas.Game[0].answers.map((data) => (
+        <div key={data.toString()}>{data}</div>
+      ));
+    } else {
+      return datas.History[0].answers.map((data) => (
+        <div key={data.toString()}>{data}</div>
+      ));
+    }
+  };
+
   return (
     <>
-      <div>{question}</div>
+      <div>{title}</div>
+      <div>{getQuestion()}</div>
+      <div>{getTexts()}</div>
+      <button>回答する</button> <br />
       <button onClick={backTop}>トップに戻る</button>
     </>
   );
--- src/dataset.js
+++ src/dataset.js
@@ -1,5 +1,46 @@
-const data = {
-  Computer: [{ question: 'computerクイズ', answers: ['1', '2', '3', '4'] }],
-  Finance: [{ question: 'financeクイズ', answers: ['a', 'i', 'u', 'e'] }],
+const dataset = {
+  Computer: [
+    {
+      question:
+        '問1 トランスポート層のプロトコルであり、信頼性よりもリアルタイム性が重視される場合に用いられるものはどれか。',
+      answers: ['HTTP', 'UDP', 'TCP', 'IP'],
+    },
+    { question: 'computerクイズ2', answers: ['1.2', '2.2', '3.2', '4.2'] },
+    { question: 'computerクイズ3', answers: ['1.3', '2.3', '3.3', '4.3'] },
+    { question: 'computerクイズ4', answers: ['1.4', '2.4', '3.4', '4.4'] },
+  ],
+  Finance: [
+    {
+      question: '実質金利とは？',
+      answers: [
+        '名目金利＋物価上昇率',
+        '名目金利ー物価上昇率',
+        '平均賃金×物価上昇率',
+        '平均賃金÷物価上昇率',
+      ],
+    },
+    { question: 'financeクイズ2', answers: ['1.2', '2.2', '3.2', '4.2'] },
+    { question: 'financeクイズ3', answers: ['1.3', '2.3', '3.3', '4.3'] },
+    { question: 'financeクイズ4', answers: ['1.4', '2.4', '3.4', '4.4'] },
+  ],
+  Game: [
+    {
+      question: 'FF10の主人公の名前は？',
+      answers: ['アーロン', 'クラウド', 'ユウナ', 'ティーダ'],
+    },
+    { question: 'computerクイズ2', answers: ['1.2', '2.2', '3.2', '4.2'] },
+    { question: 'computerクイズ3', answers: ['1.3', '2.3', '3.3', '4.3'] },
+    { question: 'computerクイズ4', answers: ['1.4', '2.4', '3.4', '4.4'] },
+  ],
+  History: [
+    {
+      question: '本能寺の変を起こした人物の名前はなんでしょう？',
+      answers: ['伊達政宗', '徳川家康', '明智光秀', '豊臣秀吉'],
+    },
+    { question: 'computerクイズ2', answers: ['1.2', '2.2', '3.2', '4.2'] },
+    { question: 'computerクイズ3', answers: ['1.3', '2.3', '3.3', '4.3'] },
+    { question: 'computerクイズ4', answers: ['1.4', '2.4', '3.4', '4.4'] },
+  ],
 };
-export default data;
+
+export default dataset;
