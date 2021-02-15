3078b6b7b39e24da8bbb8517dab0cfbee799d87e
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-02-15 19:40:41 +0900

    追加

--- src/images/_CategoryImages.js
+++ src/images/_CategoryImages.js
@@ -1,5 +1,5 @@
 //エントリーポイント
-export { default as Game } from './game2.png';
+export { default as Game } from './game.png';
 export { default as Anime } from './Anime.jpg';
 export { default as Computer } from './computer.jpg';
 export { default as Finance } from './finance.jpg';
