a24a9da4dcbba901dafb54ed1b73723e01466d12
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-07-18 13:35:24 +0900

    remove login

--- src/components/lv1/_index.js
+++ src/components/lv1/_index.js
@@ -2,7 +2,6 @@ export { default as Score } from './Score';
 export { default as QuizsTitle } from './QuizsTitle';
 export { default as BreadcrumbList } from './BreadcrumbList';
 export { default as CategoryTitle } from './CategoryTitle';
-export { default as Login } from './Login';
 export { default as Logo } from './Logo';
 export { default as QuestionAnswer } from './QuestionAnswer';
 export { default as QuestionText } from './QuestionText';
